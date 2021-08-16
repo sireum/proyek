cc9c6a02edd657faac02fd55d88d8c77c1f724d0
Robby <robby@santoslab.org>
2021-08-16 11:40:24 -0500

    More on incremental type checking.

--- jvm/src/main/scala/org/sireum/proyek/Logika.scala
+++ jvm/src/main/scala/org/sireum/proyek/Logika.scala
@@ -27,8 +27,9 @@ package org.sireum.proyek
 
 import org.sireum._
 import org.sireum.lang.FrontEnd
+import org.sireum.lang.symbol.Resolver
 import org.sireum.lang.{ast => AST}
-import org.sireum.lang.tipe.TypeHierarchy
+import org.sireum.lang.tipe.{TypeChecker, TypeHierarchy, TypeOutliner}
 import org.sireum.message.{Message, Reporter}
 import org.sireum.project._
 
@@ -37,14 +38,14 @@ object Logika {
   @datatype class VerificationInfo(val map: HashMap[String, TypeHierarchy],
                                    val files: HashMap[String, String],
                                    val messages: ISZ[Message],
+                                   val lineOpt: Option[Z],
                                    val stop: B)
 
   @datatype class LogikaModuleProcessor(val root: Os.Path,
                                         val module: Module,
                                         val par: B,
                                         val followSymLink: B,
-                                        val outDir: Os.Path,
-                                       ) extends ModuleProcessor[VerificationInfo] {
+                                        val outDir: Os.Path) extends ModuleProcessor[VerificationInfo] {
 
     @strictpure def sha3: B = F
 
@@ -63,49 +64,42 @@ object Logika {
       return firstLine.contains("#Sireum")
     }
 
-    override def process(o: VerificationInfo,
+    override def process(info: VerificationInfo,
                          shouldProcess: B,
                          dm: DependencyManager,
                          sourceFiles: ISZ[Os.Path],
                          testSourceFiles: ISZ[Os.Path]): (VerificationInfo, B) = {
-      var info = o
       val fileUriSet = HashSSet.empty[String] ++ (for (p <- sourceFiles ++ testSourceFiles) yield p.toUri)
       val checkUriSet = HashSSet.empty[String] ++ (for (p <- info.files.keys) yield Os.path(p).toUri)
-      val shouldCheck = ops.ISZOps(info.files.keys).exists((p: String) => fileUriSet.contains(p))
-      var nameMap = HashMap.empty[ISZ[String], org.sireum.lang.symbol.Info]
-      var typeMap = HashMap.empty[ISZ[String], org.sireum.lang.symbol.TypeInfo]
-      var programs = ISZ[AST.TopUnit.Program]()
       val thOpt = info.map.get(module.id)
-      thOpt match {
+      val (nameMap, typeMap, programs, info2, changed): (Resolver.NameMap, Resolver.TypeMap, ISZ[AST.TopUnit.Program], VerificationInfo, B) = thOpt match {
         case Some(th) if !shouldProcess =>
-          nameMap = th.nameMap
-          typeMap = th.typeMap
-          if (shouldCheck) {
-            for (info <- nameMap.values) {
+          var nm = th.nameMap
+          var tm = th.typeMap
+          val checkFiles = info.files -- fileUriSet.elements
+          if (checkFiles.nonEmpty) {
+            val inputs = ops.ISZOps(for (pair <- checkFiles.entries) yield FrontEnd.Input(pair._2, Some(Os.path(pair._1).toUri), 0))
+            for (info <- nm.values) {
               info.posOpt.get.uriOpt match {
-                case Some(uri) if fileUriSet.contains(uri) && checkUriSet.contains(uri)  =>
-                  nameMap = nameMap - ((info.name, info))
+                case Some(uri) if fileUriSet.contains(uri) && checkUriSet.contains(uri) => nm = nm - ((info.name, info))
                 case _ =>
               }
             }
-            for (info <- typeMap.values) {
+            for (info <- tm.values) {
               info.posOpt.get.uriOpt match {
-                case Some(uri) if fileUriSet.contains(uri) && checkUriSet.contains(uri) =>
-                  typeMap = typeMap - ((info.name, info))
+                case Some(uri) if fileUriSet.contains(uri) && checkUriSet.contains(uri) => tm = tm - ((info.name, info))
                 case _ =>
               }
             }
+            val results = ops.ISZOps(
+              if (par) inputs.parMap(FrontEnd.parseGloballyResolve _)
+              else inputs.map(FrontEnd.parseGloballyResolve _))
+            val q = results.
+              foldLeft(FrontEnd.combineParseResult _, (ISZ[Message](), ISZ[AST.TopUnit.Program](), nm, tm))
+            (q._3, q._4, q._2, info(messages = info.messages ++ q._1), T)
+          } else {
+            (nm, tm, ISZ(), info, F)
           }
-          val inputs = ops.ISZOps(for (pair <- info.files.entries) yield FrontEnd.Input(pair._2, Some(Os.path(pair._1).toUri), 0))
-          val results = ops.ISZOps(
-            if (par) inputs.parMap(FrontEnd.parseGloballyResolve _)
-            else inputs.map(FrontEnd.parseGloballyResolve _))
-          val (messages, ps, nm, tm) = results.
-            foldLeft(FrontEnd.combineParseResult _, (ISZ[Message](), ISZ[AST.TopUnit.Program](), nameMap, typeMap))
-          info = info(messages = info.messages ++ messages)
-          nameMap = nm
-          typeMap = tm
-          programs = ps
         case _ =>
           def toInput(p: Os.Path): FrontEnd.Input = {
             val uri = p.toUri
@@ -118,22 +112,53 @@ object Logika {
           val results = ops.ISZOps(
             if (par) inputs.parMap(FrontEnd.parseGloballyResolve _)
             else inputs.map(FrontEnd.parseGloballyResolve _))
-          val (messages, ps, nm, tm) = results.
-            foldLeft(FrontEnd.combineParseResult _, (ISZ[Message](), ISZ[AST.TopUnit.Program](), nameMap, typeMap))
-          info = info(messages = info.messages ++ messages)
-          nameMap = nm
-          typeMap = tm
-          programs = for (program <- ps if checkUriSet.contains(program.fileUriOpt.get)) yield program
+          val (nm, tm) = Resolver.addBuiltIns(HashMap.empty, HashMap.empty)
+          val q = results.
+            foldLeft(FrontEnd.combineParseResult _, (ISZ[Message](), ISZ[AST.TopUnit.Program](), nm, tm))
+          (q._3, q._4,
+            for (program <- q._2 if checkUriSet.contains(program.fileUriOpt.get)) yield program,
+            info(messages = info.messages ++ q._1), T)
       }
       val rep = Reporter.create
-      val th = TypeHierarchy.build(T, TypeHierarchy(nameMap, typeMap, Poset.empty, HashMap.empty), rep)
-      info = info(messages = info.messages ++ rep.messages)
-      if (ops.ISZOps(info.messages).exists((m: Message) => m.isError || m.isInternalError)) {
-        return (info(stop = T), F)
+      if (changed) {
+        var th = TypeHierarchy.build(T, TypeHierarchy(nameMap, typeMap, Poset.empty, HashMap.empty), rep)
+        if (!rep.hasError) {
+          th = TypeOutliner.checkOutline(T, T, th, rep)
+        }
+        if (!rep.hasError) {
+          var nm = HashMap.empty[ISZ[String], lang.symbol.Info]
+          var tm = HashMap.empty[ISZ[String], lang.symbol.TypeInfo]
+          if (nm.nonEmpty || tm.nonEmpty) {
+            for (info <- th.nameMap.values) {
+              info.posOpt.get.uriOpt match {
+                case Some(uri) if fileUriSet.contains(uri) && checkUriSet.contains(uri) => nm = nm + info.name ~> info
+                case _ =>
+              }
+            }
+            for (info <- th.typeMap.values) {
+              info.posOpt.get.uriOpt match {
+                case Some(uri) if fileUriSet.contains(uri) && checkUriSet.contains(uri) => tm = tm + info.name ~> info
+                case _ =>
+              }
+            }
+            th = TypeChecker.checkComponents(T, T, th, nm, tm, rep)
+          }
+        }
+        val info3 = info2(messages = info2.messages ++ rep.messages)
+        if (ops.ISZOps(info3.messages).exists((m: Message) => m.isError || m.isInternalError)) {
+          return (info3(stop = T), changed)
+        }
+        val newFiles = info2.files -- (checkUriSet -- fileUriSet.elements).elements
+        val info4 = info3(
+          map = info3.map + module.id ~> th,
+          files = newFiles,
+          stop = info3.files.nonEmpty ->: newFiles.isEmpty
+        )
+        // TODO: Verify programs
+        return (info4, changed)
+      } else {
+        return (info2, changed)
       }
-      info = info(map = info.map + module.id ~> th)
-
-      halt("TODO")
     }
   }
 
