e8fe643cb5d88044be298ad29456ed51ff16a678
Robby <robby@santoslab.org>
2021-08-28 15:11:42 -0500

    Added cvc4 and z3 specific options in Logika.

--- jvm/src/main/scala/org/sireum/proyek/LogikaProyek.scala
+++ jvm/src/main/scala/org/sireum/proyek/LogikaProyek.scala
@@ -267,7 +267,7 @@ object LogikaProyek {
         config = config,
         th = th,
         smt2f = (th: TypeHierarchy) =>
-          Smt2Impl.create(config.smt2Configs, th, config.timeoutInMs, config.charBitWidth,
+          Smt2Impl.create(config.smt2Configs, th, config.timeoutInMs, config.cvc4RLimit, config.charBitWidth,
             config.intBitWidth, config.simplifiedQuery, rep),
         cache = cache,
         reporter = rep,
