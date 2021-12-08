9e3885daf6c35fc2e3ca7ea927c7a458139c1ffc
Robby <robby@santoslab.org>
2021-12-08 09:42:23 -0600

    Generalized cvc4 to cvc to support both cvc4 and cvc5.

--- jvm/src/main/scala/org/sireum/proyek/Analysis.scala
+++ jvm/src/main/scala/org/sireum/proyek/Analysis.scala
@@ -268,7 +268,7 @@ object Analysis {
         config = config,
         th = th,
         smt2f = (th: TypeHierarchy) =>
-          Smt2Impl.create(config.smt2Configs, th, config.timeoutInMs, config.cvc4RLimit, config.fpRoundingMode,
+          Smt2Impl.create(config.smt2Configs, th, config.timeoutInMs, config.cvcRLimit, config.fpRoundingMode,
             config.charBitWidth, config.intBitWidth, config.useReal, config.simplifiedQuery,
             reporter.asInstanceOf[logika.Logika.Reporter]),
         cache = cache,
