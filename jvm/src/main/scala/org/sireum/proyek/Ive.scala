990f40e85c4ec9d25be65e47b67dd764f8bdf971
Robby <robby@santoslab.org>
2024-10-08 15:40:06 -0500

    Set Scala editor error highlighting using compiler.

--- jvm/src/main/scala/org/sireum/proyek/Ive.scala
+++ jvm/src/main/scala/org/sireum/proyek/Ive.scala
@@ -287,6 +287,7 @@ object Ive {
     val scalaSettings: ST =
       st"""<component name="ScalaProjectSettings">
           |  <option name="autoRunDelay" value="3000" />
+          |  <option name="compilerHighlightingScala2" value="true" />
           |  <option name="dontCacheCompoundTypes" value="true" />
           |  <option name="inProcessMode" value="false" />
           |  <option name="intInjectionMapping">
